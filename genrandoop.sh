b06209df4f05e8e177021060908a4f903e49aa5f
Rajesh Kumar R <rajesh.rak@gmail.com>
2016-03-08 15:48:56 +0530

    script updated to accept execution folder

--- genrandoop.sh
+++ genrandoop.sh
@@ -6,14 +6,19 @@
 # NOTE: The folder containing the list of classes need to be in the current 
 # directory and the folder name is to be sent as a parameter to this script
 # 
-# Usage: ./genrandoop -dir=<folder name> [-listfile=<class list file name>]
+# Usage: ./genrandoop -dir=<folder name> [-listfile=<class list file name>] [-execfolder=<working folder>] [-dryrun]
 #	the class list file is expected to be contained in the folder specified 
 #	by the -dir parameter. If not specified it will be taken as classlist.txt
+#       wfolder is the parameter to specify a working folder under which the folder
+#       specified by -dir parameter is present
+# 	if dryrun is specified then the actual execution will not be performed
 pwdir=$(pwd)
 
 delim="="
 listfile="classlist.txt"
 wfolder="NOT-SPECIFIED"
+execfolder="NOT-SPECIFIED"
+dryrun="FALSE"
 
 for var in "$@"
 do
@@ -26,9 +31,13 @@ do
 	
 	elif [ "$inparam" == "-listfile" ]
 	then
-   		
-		echo "inside -listfile"		
 		listfile=$invalue
+	elif [ "$inparam" == "-execfolder" ]
+	then
+		execfolder=$invalue
+	elif [ "$inparam" == "-dryrun" ]
+	then
+		dryrun="TRUE"
 	else
    		echo "Unrecognized Parameter: $inparam"
 	fi
@@ -45,15 +54,31 @@ export CLASSPATH=$pwdir:$pwdir/$wfolder:$pwdir/lib/sootall-2.3.0.jar:$pwdir/lib/
 
 echo $listfile
 echo $wfolder
-printenv CLASSPATH
 
+if [ "$execfolder" == "NOT-SPECIFIED" ]
+then
+	echo "Executing under current folder"
+else 
+	cd $execfolder
+	echo "Executing under folder: $execfolder"
+fi
 
 
 cd $wfolder
+ls
+echo "PWD:"$(pwd)
 
 echo ""
 echo "Attempting to generate Randoop Tests..."
 
+echo "Dry Run: $dryrun"
+if [ "$dryrun" == "FALSE" ]
+then
+#{
 java -Xms2048m -Xmx4096m randoop.main.Main gentests --classlist=$listfile --timelimit=30 --inputlimit=50 --outputlimit=40
-cd ..
+#}
+else
+	echo "...dry run...no exec ..."
+fi
+
 echo "...done"
