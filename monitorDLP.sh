3e62c8c458c39d41aa6b0a11704c72b895c497f3
rajesh-rak <rajesh.rak@gmail.com>
2017-12-09 23:07:15 +0530

    Update to catch deadlocks during Test case generation

--- monitorDLP.sh
+++ monitorDLP.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 # @Author: Rajesh Kumar R (rajesh.rak@gmail.com) IIIT-B MS2013007
-# 
+#
 # This script will monitor the running DLP program for any unrecoverable deadlocks
 # in case such dead locks are found it will log the stack trace kill the deadlocked
 # process and log the results. The script will exit when the runDLP script
 # has completed its execution or no dead lock prediction program is runing
-# 
+#
 # The algorithm is as follows:
 # 1. Check the java processes that are being executed
 # 2. If the java processes of the dead lock detection routines are having the same PID
@@ -14,11 +14,11 @@
 # 3. Java stack traces are taken for the deadlock detection programs
 # 4. If the JNI Glabal references are remaining the same for the designated n. of tries
 #    the program is assumed to be locked
-# 5. The JNI Global reference check is made for all detection programs - to be sure that 
+# 5. The JNI Global reference check is made for all detection programs - to be sure that
 #    the code is dead locked
 # 6. A delay is introduced that is configurable between multiple tries
 #
-# Usage ./monitorDLP.sh 
+# Usage ./monitorDLP.sh
 #
 #
 #21091 OmenDriver -Dfile.encoding=ISO-8859-1 -Xms2048m -Xmx3072m -ea
@@ -42,7 +42,9 @@ omen=OmenDriver
 
 run_folder=scanresultfolder.out
 output_folder=outfolder.txt
+testFolder=dlp_test_folder.txt
 analysis_file=AnalysisData_w.txt
+testhung_file=TestGenHung.txt
 
 Y=1
 N=0
@@ -53,7 +55,7 @@ lockedPIDs=()
 log_pid_file() {
 	jps -v > $pid_file1
 	sleep $DELAY_SECS
-	jps -v > $pid_file2	
+	jps -v > $pid_file2
 }
 
 # Read the file in parameter and fill the array named "array"
@@ -61,26 +63,26 @@ getFile1Pids() {
     array1=() # Create array
     while IFS= read -r line # Read a line
     do
-        array1+=("$line") 
+        array1+=("$line")
     done < "$pid_file1"
 }
 getFile2Pids() {
     array2=() # Create array
     while IFS= read -r line # Read a line
     do
-        array2+=("$line") 
+        array2+=("$line")
     done < "$pid_file2"
 }
 
 check_pid_match() { # populates matchesP and mathesID array if the process ids match
-	
+
 	java_running=$N
-	
+
 	getFile1Pids
 	getFile2Pids
 	matchesP=() # Array to hold the process name matches
 	matchesID=() # Array to hold the process ID matches
-	
+
 	icount1=${#array1[*]}
 	icount2=${#array2[*]}
 	for (( i=0; i < $icount1; i++ ))
@@ -89,28 +91,28 @@ check_pid_match() { # populates matchesP and mathesID array if the process ids m
 		idt1=$(echo $pidtxt1|cut -d ' ' -f 1)
 		proc1=$(echo $pidtxt1|cut -d ' ' -f 2)
 
-		
+
 		#ignore Jps
-		if [ "$proc1" = "$jps_txt" ] 
+		if [ "$proc1" = "$jps_txt" ]
 		then
 			continue
-		fi	
-	
-		java_running=$Y
+		fi
 
+		java_running=$Y
+		retry_count=0
 		for (( j=0; j < $icount2; j++ ))
 		do
 			pidtxt2=${array2[$j]}
 			idt2=$(echo $pidtxt2|cut -d ' ' -f 1)
 			proc2=$(echo $pidtxt2|cut -d ' ' -f 2)
-			
+
 			if [ "$proc1" = "$proc2" ] && [ "$idt1" = "$idt2" ]
 			then
 				matchesP+=("$proc1")
 				matchesID+=("$idt1")
 			fi
 		done
-	done	
+	done
 }
 
 logjStack() {
@@ -126,26 +128,59 @@ logjStack() {
 
 process_report()   {
     outfolder_present=$([ -f $output_folder ] && echo "Y" || echo "N")
-    if [ "$outfolder_present" = "Y" ]
+		testfolder_present=$([ -f $testFolder ] && echo "Y" || echo "N")
+
+		if [ "$testfolder_present" = "N" ]
+		then
+	        echo Test folder does on exist, not processing results!
+	        return 0
+		fi
+
+		if [ "$outfolder_present" = "Y" ]
     then
+        testhung_flag="N"
         outfolder=$(head -n 1 $output_folder)
-        analysis_outfile=$outfolder/$analysis_file
-        echo "Java processes in indefinite wait:" > $analysis_outfile
-        count=${#matchesID[*]}
-	    for (( k=0; k < $count; k++ ))
-	    do
-	        procID=${matchesID[$k]}
-		    procName=${matchesP[$k]}
-		    echo "Process Name: $procName" >> $analysis_outfile
-		    proc_stack_file=$(echo $procName"_stack.txt")
-		    jstack -l $procID > $outfolder/$proc_stack_file
-	    done
-    else 
-        echo Outout Folder not present, not processing results!
+        echo -n "_"
+    else
+        testhung_flag="Y"
+        outfolder=$execfolder/output/$dlpTestFolder
     fi
+
+	  if [ ! -d "$execfolder" ]
+		then
+	        echo Runfolder does on exist, not processing results!
+	        return 0
+	    elif [ ! -d "$execfolder/output" ]
+		then
+		    mkdir $execfolder/output
+		    mkdir $execfolder/output/$dlpTestFolder
+		elif [ ! -d "$execfolder/output/$dlpTestFolder" ]
+		then
+		    mkdir $execfolder/output/$dlpTestFolder
+		fi
+
+    analysis_outfile=$outfolder/$analysis_file
+    testhung_outfile=$outfolder/$testhung_file
+    if [ "$testhung_flag" = "Y" ]
+    then
+        echo "Test generation Hung:" >> $analysis_outfile
+        echo "Test generation Hung" >> $testhung_outfile
+    else
+        echo "Test execution Hung:" >> $analysis_outfile
+    fi
+
+    count=${#matchesID[*]}
+    for (( k=0; k < $count; k++ ))
+    do
+      procID=${matchesID[$k]}
+	    procName=${matchesP[$k]}
+	    echo "Process Name: $procName" >> $analysis_outfile
+	    proc_stack_file=$(echo $procName"_stack.txt")
+	    jstack -l $procID > $outfolder/$proc_stack_file
+    done
 }
 
-detect_n_kill_locked_process() { 
+detect_n_kill_locked_process() {
 	count=${#matchesID[*]}
 	report_flag="N"
 	for (( j=0; j < $count; j++ ))
@@ -157,7 +192,7 @@ detect_n_kill_locked_process() {
 		then
 			java_running=$Y
 			continue
-		fi	
+		fi
 		#get java stack trace
 		logjStack $pidtxt
 		jgr1txt=$(grep "JNI global references" jstack1.txt)
@@ -169,12 +204,12 @@ detect_n_kill_locked_process() {
 		stack_bytes1=$(wc jstack1.txt | awk '{print $3}')
 		stack_bytes2=$(wc jstack2.txt | awk '{print $3}')
 
-		if [ "$jgr1" = "$jgr2" ] && [ "$stack_wc1" = "$stack_wc2" ] && [ "$stack_bytes1" = "$stack_bytes2" ] 
+		if [ "$jgr1" = "$jgr2" ] && [ "$stack_wc1" = "$stack_wc2" ] && [ "$stack_bytes1" = "$stack_bytes2" ]
 		then
 			echo -n "#"
 			#process report if any of the process is hung, to be done only once
 			#as reporting happens for all processes that are running
-			if [ "$report_flag" = "N" ] 
+			if [ "$report_flag" = "N" ]
 			then
 			    process_report
 			    report_flag="Y"
@@ -182,30 +217,42 @@ detect_n_kill_locked_process() {
 			kill -9 $pidtxt
 		fi
 	done
-	
+
 }
 
-
+monitor_for_deadlock() {
+	while [ $java_running -eq $Y ]
+	do
+		echo -n "."
+		execfolder=$(head -n 1 $run_folder)
+		dlpTestFolder=$(head -n 1 $testFolder)
+		lockedPIDs=()
+		loop_count+=1
+		log_pid_file
+		check_pid_match
+		matchPCount=${#matchesP[*]}
+		matchIDCount=${#matchesID[*]}
+		if [ $matchPCount -gt 0 ]
+		then
+			echo -n "*"
+			detect_n_kill_locked_process
+		fi
+		sleep $LOOP_WAIT_SECS
+	done
+}
 
 echo -n "Monitoring Java processes..."
-echo 
+echo
 #uncomment to stop processing this script - for debigging purpose
 #java_running=0
-
-while [ $java_running -eq $Y ]
+execfolder=$(head -n 1 $run_folder)
+dlpTestFolder=$(head -n 1 $testFolder)
+retry_count=0
+while [ $retry_count -lt $RE_TRIES ]
 do
-	echo -n "."
-	lockedPIDs=()
-	loop_count+=1
-	log_pid_file
-	check_pid_match
-	matchPCount=${#matchesP[*]}
-	matchIDCount=${#matchesID[*]}
-	if [ $matchPCount -gt 0 ]
-	then
-		echo -n "*"
-		detect_n_kill_locked_process
-	fi	
+	monitor_for_deadlock
+	retry_count+=1
+	java_running=$Y
 	sleep $LOOP_WAIT_SECS
 done
 echo "No more Java processes to monitor"
@@ -215,14 +262,5 @@ rm -f $pid_file1
 rm -f $pid_file2
 rm -f jstack1.txt
 rm -f jstack2.txt
- 
+
 echo "Done, Monitoring concluded"
-
-
-
-
-
-
-
-
-
--- runDLP.sh
+++ runDLP.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 #
 # @Author: Rajesh Kumar R (rajesh.rak@gmail.com) IIIT-B MS2013007
-# 
-# This script will run all the necessary programs and scripts to detect a 
+#
+# This script will run all the necessary programs and scripts to detect a
 # deadlock. Here are the things that will be done by this script:
 #
-# 1. Scan the predictions made by stalemate tool. The stalemate tool itself is 
+# 1. Scan the predictions made by stalemate tool. The stalemate tool itself is
 #    not run as part of this script, but the prediction files in the 'cycles'
-#    folder  will be scanned and  set of classes will be shortlisted to examine 
+#    folder  will be scanned and  set of classes will be shortlisted to examine
 #    for deadlocks, based on the criteria specified in the dlpscanner.properties
 #    IMPORTANT: make sure the dlpscanner.properties is set correctly before
 #    runnning this script.
-# 
+#
 # 2. The shortlisted classes list will be created in a separate folders
-#    and under a execution folder. This execution folder will be fed to the 
-#    Randoop generator so that test cases will be generated for each of the 
+#    and under a execution folder. This execution folder will be fed to the
+#    Randoop generator so that test cases will be generated for each of the
 #    shortlisted classes by Randoop
 #
 # 3. The generated test cases will be then fed into the Omen+ tool, which will
 #    analyze these test cases for  creation of deadlocking scenarios and if such
-#    a scenario is detected, a deadlocking test case will be generated. All the 
-#    outcome of the Omen+ tool will be kept under the 'output' folder under the 
+#    a scenario is detected, a deadlocking test case will be generated. All the
+#    outcome of the Omen+ tool will be kept under the 'output' folder under the
 #    execution folder
 #
-# 4. The output folder will be examined for any detected deadlocks and a 
-#    summary output will be generated 
-# 
-# The above tasks are all achieved by multiple programs and multiple scripts, 
-# and this script brings them all together so that this automation is 
+# 4. The output folder will be examined for any detected deadlocks and a
+#    summary output will be generated
+#
+# The above tasks are all achieved by multiple programs and multiple scripts,
+# and this script brings them all together so that this automation is
 # comprehensively achieved and very large set of classes and libraries can be
-# examined for deadlocks. 
-# This set of tools and automation provides a definitive way of detecting 
+# examined for deadlocks.
+# This set of tools and automation provides a definitive way of detecting
 # deadlocks in large libraries which is otherwise not possible with the existing
-# methods and tools available in the industry and in the research community 
+# methods and tools available in the industry and in the research community
 # (March - 2016)
 #
 # -------------------------------------------
@@ -45,13 +45,14 @@
 # Usage ./runDLP.sh [-dryrun], if -dryrun parameter is specified, no actual
 #				execution will be done
 #
-DRY_RUN="OFF" 
+DRY_RUN="OFF"
 inputFile="scanresultfolder.out"
 echo "DLP initiated..."
 execFolder="NOT SPECIFIED"
 pDir="NOT SPECIFIED"
 execLogFile="exec_log.txt"
 summaryOutFile="DLP_Execution_Report.csv"
+testFolder="dlp_test_folder.txt"
 
 for var in "$@"
 do
@@ -86,7 +87,7 @@ IFS=$'\n'
 for outFolder in $(cat "./$inputFile")
 do
 	execFolder=$outFolder
-done 
+done
 echo "Processing Folder:...$execFolder"
 
 for dirName in $execFolder/*
@@ -98,41 +99,50 @@ do
 	if [ -d ${dirName} ]
 	then
 		pDir=${dirName##*/}
+		echo $pDir > $testFolder
 		echo "--Generating Randoop Test cases for: $pDir--"
 		if [ ! $DRY_RUN == "ON" ];then
-			./genrandoop.sh -dir=$pDir -execfolder=$execFolder 
+			./genrandoop.sh -dir=$pDir -execfolder=$execFolder
 		else
 			./genrandoop.sh -dir=$pDir -execfolder=$execFolder -dryrun
-		fi	
+		fi
 		echo
 		echo
 		echo "--Tests Generated...executing Omen+--"
 		if [ ! $DRY_RUN == "ON" ];then
-			./runomen.sh -c -testdir=$pDir -execfolder=$execFolder 
+			./runomen.sh -c -testdir=$pDir -execfolder=$execFolder
 		else
 			./runomen.sh -c -testdir=$pDir -execfolder=$execFolder -dryrun
-		fi	
+		fi
 
 	fi
 done
-
+rm -f $testFolder
 if [ $DRY_RUN == "ON" ];then
 	echo "DLP Execution Completed! (Dry Run)"
 	exit 0
-fi	
+fi
 
 #Summarize Result:
 tresultfile="tmpresult.txt"
 ttestfile="tmptest.txt"
 resultfile="DLP_Result.txt"
 hunglist="hunglist.txt"
+testhunglist="testhunglist.txt"
+analysis_file="Analysis"
+hung_file="AnalysisData_w.txt"
+testhung_file="TestGenHung.txt"
+
 cd $execFolder/output
-find . -name Analysis* > $tresultfile
-find . -name AnalysisData_w.txt > $hunglist
+find . -name $analysis_file* > $tresultfile
+find . -name $hung_file > $hunglist
+find . -name $testhung_file > $testhunglist
+
 ls -d Test*/ > $ttestfile
 testcount=0
 positives=0
 hungcount=0
+testhungcount=0
 
 while read line
 do
@@ -146,10 +156,13 @@ while read line
 do
 	((hungcount++))
 done < $hunglist
-
+while read line
+do
+	((testhungcount++))
+done < $testhunglist
 echo -e "DLP Execution Summary" >> $resultfile
 echo -e "---------------------" >> $resultfile
-echo -e "Total Test(s) executed\t\t: $testcount" 
+echo -e "Total Test(s) executed\t\t: $testcount"
 echo -e "Total Test(s) executed\t\t: $testcount" >> $resultfile
 
 echo -e "Total Dead-Lock(s) detected\t: $positives"
@@ -177,9 +190,23 @@ then
         	echo $hxline >> $resultfile
         done < $hunglist
     fi
+    if [ $testhungcount -gt 0 ]
+    then
+        echo -e "----------------------" >> $resultfile
+        echo -e "Hung Test Generations:" >> $resultfile
+        echo -e "----------------------" >> $resultfile
+        while read line
+        do
+        	ttline=${line#*/}
+        	txline=${ttline%/*}
+        	echo $txline >> $resultfile
+        done < $testhunglist
+    fi
 fi
 
 rm -f $tresultfile
+rm -f $hunglist
+rm -f $testhunglist
 rm -f $ttestfile
 mv $resultfile ../
 
@@ -189,7 +216,7 @@ runID=${execFolder##*/}
 if [ ! -f "$outFolder/$summaryOutFile" ]
 then
 	echo "No Summary File"
-	echo -e "RunID,Tests,Dead_Locks,Progs_Hung,NH_Depth,NH_Density,Nodes_Parsed,Nodes_Shortlisted,Duplicates,Skipped,Time_Stamp,Scan_Files" > $outFolder/$summaryOutFile
+	echo -e "RunID,Tests,Dead_Locks,Progs_Hung,TestGen_Hung,NH_Depth,NH_Density,Nodes_Parsed,Nodes_Shortlisted,Duplicates,Skipped,Time_Stamp,Scan_Files" > $outFolder/$summaryOutFile
 fi
 nhDepth=""
 nhDensity=""
@@ -206,14 +233,14 @@ while read rLine
 do
 	if [[ "$rLine" =~ "Neighbourhood Depth" ]]
 	then
-		nhDepth=${rLine##*:} 
+		nhDepth=${rLine##*:}
 		echo "NH: depth:$nhDepth"
-			
+
 	elif [[ "$rLine" =~ "Neighbourhood Density" ]]
 	then
 		nhDensity=${rLine##*:}
 		echo "NH: density:$nhDensity"
-		
+
 	elif [[ "$rLine" =~ "Total Nodes Processed" ]]
 	then
 		nodesParsed=${rLine##*:}
@@ -221,7 +248,7 @@ do
 	elif [[ "$rLine" =~ "Total Nodes Selected" ]]
 	then
 		nodesShortlisted=${rLine##*:}
-	    nodesShortlisted=$nodesShortlisted 
+	    nodesShortlisted=$nodesShortlisted
 	elif [[ "$rLine" =~ "File" ]]
 	then
 		filenames+=${rLine##*:}
@@ -229,16 +256,10 @@ do
 	elif [[ "$rLine" =~ "Total Duplicates" ]]
 	then
 		dupNodes+=${rLine##*:}
-	    dupNodes=$dupNodes 
-	
+	    dupNodes=$dupNodes
+
 	fi
 done < $execFolder/$execLogFile
 
 nodesSkipped=$[$nodesParsed - $nodesShortlisted - $dupNodes]
-		echo "$runID,$testcount,$positives,$hungcount,$nhDepth,$nhDensity,$nodesParsed,$nodesShortlisted,$dupNodes,$nodesSkipped,$timeStamp,$filenames" | tr -d '\t' >> $outFolder/$summaryOutFile
-
-
-
-
-
-
+		echo "$runID,$testcount,$positives,$hungcount,$testhungcount,$nhDepth,$nhDensity,$nodesParsed,$nodesShortlisted,$dupNodes,$nodesSkipped,$timeStamp,$filenames" | tr -d '\t' >> $outFolder/$summaryOutFile
